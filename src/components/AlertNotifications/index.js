6081c413fcf61ba2f9fdd5fd0752231bf15b6dc7
CA-MANOJ-VARMA <camanojvarma@gmail.com>
2023-02-27 17:23:00 +0000

    FEB 27

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -1,29 +1,36 @@
 // Write your code here
 import './index.css'
-
+import {AiFillCheckCircle} from 'react-icons/ai'
+import {RiErrorWarningFill} from 'react-icons/ri'
+import {MdWarning, MdInfo} from 'react-icons/md'
+import {GrFormClose} from 'react-icons/gr'
 import Notification from '../Notification'
 
 const NotificationArray = [
   {
-    icon: 'AiFillCheckCircle',
+    icon: <AiFillCheckCircle />,
     type: 'Success',
     description: 'You can access all the files in the folder',
+    color: '#2dca73',
   },
   {
-    icon: 'RiErrorWarningFill',
+    icon: <RiErrorWarningFill />,
     type: 'Error',
     description:
       'Sorry, you are not authorized to have access to delete the file',
+    color: '#ff0b37',
   },
   {
-    icon: 'MdWarning',
+    icon: <MdWarning />,
     type: 'Warning',
     description: 'Viewers of this file can see comments and suggestions',
+    color: '#ffb800',
   },
   {
-    icon: 'MdInfo',
+    icon: <MdInfo />,
     type: 'Info',
     description: 'Anyone on the internet can view these files',
+    color: '#0f81e0',
   },
 ]
 
@@ -34,7 +41,25 @@ const AlertNotifications = () => {
       <h1>Alert Notifications</h1>
       <ul className="css-ul-container">
         {NotificationArray.map(eachItem => (
-          <Notification NotificationElement={eachItem} key={eachItem.type} />
+          <Notification>
+            <li key={eachItem.type}>
+              <div className="css-notification-container">
+                <div>
+                  <div
+                    className="css-icon-container"
+                    style={{color: eachItem.color}}
+                  >
+                    {eachItem.icon}
+                    <h1>{eachItem.type}</h1>
+                  </div>
+                  <p>{eachItem.description}</p>
+                </div>
+                <div>
+                  <GrFormClose />
+                </div>
+              </div>
+            </li>
+          </Notification>
         ))}
       </ul>
     </div>
--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,62 +1,9 @@
 // Write your code here
-import {AiFillCheckCircle} from 'react-icons/ai'
-import {RiErrorWarningFill} from 'react-icons/ri'
-import {MdWarning, MdInfo} from 'react-icons/md'
-import {GrFormClose} from 'react-icons/gr'
 import './index.css'
 
 const Notification = props => {
-  const {NotificationElement} = props
-
-  const IconFunction = () => {
-    console.log('icon')
-    switch (NotificationElement.icon) {
-      case 'AiFillCheckCircle':
-        return (
-          <div className="css-icon-container" style={{color: '#2dca73'}}>
-            <AiFillCheckCircle />
-            <h1>{NotificationElement.type}</h1>
-          </div>
-        )
-      case 'RiErrorWarningFill':
-        return (
-          <div className="css-icon-container" style={{color: '#ff0b37'}}>
-            <RiErrorWarningFill />
-            <h1>{NotificationElement.type}</h1>
-          </div>
-        )
-      case 'MdWarning':
-        return (
-          <div className="css-icon-container" style={{color: '#ffb800'}}>
-            <MdWarning />
-            <h1>{NotificationElement.type}</h1>
-          </div>
-        )
-      case 'MdInfo':
-        return (
-          <div className="css-icon-container" style={{color: '#0f81e0'}}>
-            <MdInfo />
-            <h1>{NotificationElement.type}</h1>
-          </div>
-        )
-      default:
-        return null
-    }
-  }
-
-  return (
-    <li>
-      <div className="css-notification-container">
-        <div>
-          {IconFunction()}
-          <p>{NotificationElement.description}</p>
-        </div>
-        <div>
-          <GrFormClose />
-        </div>
-      </div>
-    </li>
-  )
+  console.log(props)
+  return <div>{props.children}</div>
 }
 
 export default Notification
